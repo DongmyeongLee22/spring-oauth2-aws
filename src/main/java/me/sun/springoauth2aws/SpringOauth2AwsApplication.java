1c2c836b215e6fc532afa6e528f480126d0b2485
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-07 01:14:20 +0900

    Initial commit

---
+++ src/main/java/me/sun/springoauth2aws/SpringOauth2AwsApplication.java
@@ -0,0 +1,13 @@
+package me.sun.springoauth2aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2AwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2AwsApplication.class, args);
+    }
+
+}
